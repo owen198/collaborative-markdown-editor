842bfac8cc289d62599f782b6b82b2c3b200ff9e
yanpin <teechieh7@gmail.com>
2016-04-11 23:04:04 +0800

    Server code 整理
    我將原本的內文部分改短了，並整理Socket.Io 的廣播狀況並寫上範例。
    路由設定部分我將它設定成可讀取Url 的方式。

--- server.js
+++ server.js
@@ -1,85 +1,98 @@
-var ServeStaticLibrary = require('serve-static');        
 var ExpressLibrary = require('express');                 
+var ExpressClient = ExpressLibrary();                           
+var ExpressRouter = ExpressLibrary.Router();
+
+var ServeStaticLibrary = require('serve-static');        
 var PathLibrary = require('path');                       
 var HttpLibrary = require('http');                       
 
 var OtLibrary = require('ot');                           
 var SocketIoLibrary = require('socket.io');              
 
-var ExpressClient = ExpressLibrary();                           
 var HttpClientServer = HttpLibrary.createServer(ExpressClient);
 
-var CodeValue = 
-"# (GitHub-Flavored) Markdown Editor\n" +
-"\n" +
-"Basic useful feature list:\n" +
-"\n" +
-" * Ctrl+S / Cmd+S to save the file\n" +
-" * Ctrl+Shift+S / Cmd+Shift+S to choose to save as Markdown or HTML\n" +
-" * Drag and drop a file into here to load it\n" +
-" * File contents are saved in the URL so you can share files\n" +
-"\n" +
-"\n" +
-"I'm no good at writing sample / filler text, so go write something yourself.\n" +
-"\n" +
-"Look, a list!\n" +
-"\n" +
-" * foo\n" +
-" * bar\n" +
-" * baz\n" +
-"\n" +
-"And here's some code! :+1:\n" +
-"\n" +
-"```javascript\n" +
-"$(function(){\n" +
-"  $('div').html('I am a div.');\n" +
-"});\n" +
-"```\n" +
-"\n" +
-"This is [on GitHub](https://github.com/jbt/markdown-editor) so let me know if I've b0rked it somewhere.\n" +
-"\n" +
-"Props to Mr. Doob and his [code editor](http://mrdoob.com/projects/code-editor/), from which\n" +
-"the inspiration to this, and some handy implementation hints, came.\n" +
-"\n" +
-"### Stuff used to make this:\n" +
-"\n" +
-" * [markdown-it](https://github.com/markdown-it/markdown-it) for Markdown parsing\n" +
-" * [CodeMirror](http://codemirror.net/) for the awesome syntax-highlighted editor\n" +
-" * [highlight.js](http://softwaremaniacs.org/soft/highlight/en/) for syntax highlighting in output code blocks\n" +
-" * [js-deflate](https://github.com/dankogai/js-deflate) for gzipping of data to make it fit in URLs\n";
+//路由設定====================================================
+//連線到http://localhost:5000 會將檔案index.html給User
+ExpressRouter.get('/', function(req, res) {
+  FileKey = req.query;	
+  //Url 的資料
+  res.sendFile(PathLibrary.join(__dirname + '/index.html'));	
+})
+ExpressClient.use(ExpressLibrary.static(__dirname + '/'));
+ExpressClient.use('/', ExpressRouter);
+//=====================================================
 
-//=====================================================
-ExpressClient.use('/', ServeStaticLibrary(PathLibrary.join(__dirname, '/')));
-ExpressClient.use('/', ServeStaticLibrary(PathLibrary.join(__dirname, '/')));
-//=====================================================
+var CodeValue = "ke it fit in URLs";							
+//File Code		
 var SocketoIoListen = SocketIoLibrary.listen(HttpClientServer);
-var SocketIoServer = 
-  new OtLibrary.EditorSocketIOServer(
-    CodeValue, 
-    [], 
-    'Text', 
-    function(socket, ClientData){
+//將 SocketoIo 啟動並綁在Http服務上
+
+
+//function 位置 editor-socketio-server.js
+var Ot = 
+ new OtLibrary.EditorSocketIOServer(
+    CodeValue, 									//document
+    [], 										//operations
+    'text', 									//docId
+    function(socket, ClientData){				//mayWrite
       ClientData(!!socket.mayEdit);
     }
   );
-
-
 //=====================================================
+var roomUser = {  };
 SocketoIoListen.on('connection', function (socket){
-  SocketIoServer.addClient(socket);
-  socket.on('UserLogin', function (data){
-    socket.mayEdit = true;
-    SocketIoServer.setName(socket, data.name);
+  //處理Url Get Data 取id		
+  var Url = socket.request.headers.referer;                
+  var GetData = Url.split('?id=');   
+  var GetDataId = GetData[GetData.length-1] || 'index';       
+  var user = '';
+  //===============================================
+
+  socket.on('join', function (username) {
+    user = username;        
     
-    if(socket.emit("UserLoginData", {})){
-      console.log("OK");
-    }else{
-      console.log("Error");
+    if (!roomUser[GetDataId]) {    
+      roomUser[GetDataId] = [];
     }
-  })
+
+    roomUser[GetDataId].push(user);
+  	socket.join(GetDataId);											
+    //新增一個分流 用 Url id 當名稱
+    socket.to(GetDataId).emit('UserLogin', user.name + '加入編輯');	
+	//UserLogin 是連接口名稱
+	//GetDataId 連接口的分流名稱
+	//範例解釋:
+		//進入順序由上而下
+		//User1 連線的Url是 http://localhost:5000/?id=1234
+		//User2 連線的Url是 http://localhost:5000/?id=1234
+		//User3 連線的Url是 http://localhost:5000/?id=ABCD
+			
+			// User1 會看到 User2 進入房間 但是不會看到 User3 ，因為to(GetDataId)不同所以分流不同
+			// 同理User3不會看到User1 and User2 的狀態
+
+    socket.mayEdit = true;				//Ot 需要將socket.mayEdit 設定為 true
+    Ot.setName(socket, user.name);  	//將 socket 的設定資料送給Ot 做執行 ,
+  	Ot.addClient(socket);				//將 socket 的設定資料送給Ot 做執行	
+  });
+  
+  //斷開處理
+  socket.on('disconnect', function () {
+    socket.leave(GetDataId, function (err) {
+      if (err) {
+          log.error(err);
+      } else {
+          var index = roomUser[GetDataId].indexOf(user);
+          if (index !== -1) {
+              roomUser[GetDataId].splice(index, 1);
+              socket.to(GetDataId).emit('UserLogin',user.name+'結束編輯');
+          	  //原理相同
+          } 
+      }
+    });
+  });  
 })
 
-
 //================================================
-HttpClientServer.listen(3000, function (Port) { });
+	//Port設定
+HttpClientServer.listen(5000, function (Port) { });
 process.on('uncaughtException', function (exc) { });
